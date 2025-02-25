4d68c9696f2f38004b4d9e5a492cd174849c9b48
Lucas Bazemore <lucasabazemore@gmail.com>
2025-02-25 12:07:25 -0600

    Update Types

--- src/types/database/supabase.ts
+++ src/types/database/supabase.ts
@@ -141,24 +141,18 @@ export type Database = {
         Row: {
           created_at: string
           id: string
-          owner: string
-          owner_id: string | null
           updated_at: string
           user_id: string
         }
         Insert: {
           created_at?: string
           id?: string
-          owner: string
-          owner_id?: string | null
           updated_at?: string
           user_id: string
         }
         Update: {
           created_at?: string
           id?: string
-          owner?: string
-          owner_id?: string | null
           updated_at?: string
           user_id?: string
         }
